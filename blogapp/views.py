73218d36a7fa02e7efe71d049d3d2417c38b5700
Eric Loc <eloc1385@gmail.com>
2021-01-21 18:26:23 -0500

    updated views.py

--- blogapp/views.py
+++ blogapp/views.py
@@ -6,6 +6,6 @@ from .models import Post
 
 
 def post_list(request):
-    posts = Post.objects.all().order_by('-published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     stuff_for_frontend = {'posts': posts}
     return render(request, 'blogapp/post_list.html', stuff_for_frontend)
